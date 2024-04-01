fc2d84651810bc464c534bf33cb8d03f874f0897
August Raack <sum-rock@rockyhillhouse.org>
2024-03-31 21:33:38 -0500

    More Language Servers

--- src/lua/plugins/astroui.lua
+++ src/lua/plugins/astroui.lua
@@ -7,27 +7,5 @@ return {
   ---@type AstroUIOpts
   opts = {
     colorscheme = "catppuccin",
-    -- -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    -- highlights = {
-    --   init = { -- this table overrides highlights in all themes
-    --     -- Normal = { bg = "#000000" },
-    --   },
-    --   astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
-    --     -- Normal = { bg = "#000000" },
-    --   },
-    -- },
-    -- icons = {
-    --   -- configure the loading of the lsp in the status line
-    --   LSPLoading1 = "⠋",
-    --   LSPLoading2 = "⠙",
-    --   LSPLoading3 = "⠹",
-    --   LSPLoading4 = "⠸",
-    --   LSPLoading5 = "⠼",
-    --   LSPLoading6 = "⠴",
-    --   LSPLoading7 = "⠦",
-    --   LSPLoading8 = "⠧",
-    --   LSPLoading9 = "⠇",
-    --   LSPLoading10 = "⠏",
-    -- },
   },
 }
