0d6735b328dd399694bfb1b36e6dd3b1a6f6fe5c
August Raack <sum-rock@rockyhillhouse.org>
2025-01-25 23:17:31 -0600

    Abandon FHS

--- sum-astro-nvim/nixos.nix
+++ sum-astro-nvim/nixos.nix
@@ -4,14 +4,10 @@
     ./common.nix
     ./fonts.nix
   ];
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    neovim-unwrapped
-  ];
-  # programs.neovim = {
-  #   enable = true;
-  #   package = pkgs.neovim-unwrapped;
-  #   defaultEditor = true;
-  # };
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-unwrapped;
+    defaultEditor = true;
+  };
 
 }
